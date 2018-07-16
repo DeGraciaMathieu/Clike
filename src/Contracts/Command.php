1b118d5a64aeb38d8a33d6df4765fdb67f301699
DeGraciaMathieu <contact@degracia-mathieu.fr>
2018-07-16 19:34:08 +0200

    Improve Command contract

--- src/Contracts/Command.php
+++ src/Contracts/Command.php
@@ -3,9 +3,35 @@
 namespace DeGraciaMathieu\Clike\Contracts;
 
 interface Command {
+
+    /**
+     * Get the command description
+     * @return \DeGraciaMathieu\Clike\Contracts\Line
+     */	
     public function description() :Line;
+
+    /**
+     * Check if the command is executable
+     * @return boolean
+     */    
     public function authorized() :bool;
+
+    /**
+     * Bind of this command
+     * @return string
+     */    
     public function binding() :string;
+
+
+    /**
+     * Code executed by this command
+     * @return void
+     */    
     public function process();
+
+    /**
+     * Output of this command
+     * @return \DeGraciaMathieu\Clike\Contracts\Line[]
+     */    
     public function output() :array;
 }
